84a4fbb1ca716ddc103bd49bde88d379e7cbfcb4
Влад <poputchik07@mail.ru>
2020-08-11 20:21:23 +0300

    realization Task

--- src/TaskForce.php
+++ src/TaskForce.php
@@ -1,35 +1,70 @@
 <?php
-namespace TaskForce;
 
-class Task {
+namespace scr\TaskForce;
 
+class TaskForce implements TaskInterface
+{
     const STATUS_NEW = 'New';
     const STATUS_CANCEL = 'Cancel';
     const STATUS_WORK = 'Work';
     const STATUS_READY = 'Ready';
     const STATUS_FAILED = 'Failed';
 
-    public $activeStatus = Task::NEW_TASK;
 
-    public function status (){
-    /*определение активного статуса и вывод названия
+    private $_idPerformer;
+    private $_idClient;
 
-    'New' => 'Новое',
-    'Cancel' => 'Отменено',
-    'Work' => 'В работе',
-    'Ready' => 'Выполнено',
-    'Failed' => 'Провалено' */
+    public $_activeStatus = TaskForce::STATUS_NEW;
+
+    public function __construct (int $idPerformer, int $idClient) {
+        $this->_idPerformer = $idPerformer;
+        $this->_idClient = $idClient;
     }
 
-    public function availableStatus (){
-        //определение возможного действия
+    public function statusMap(string $status = '')
+    {
+        $statusMap = [
+            TaskForce::STATUS_NEW => "Новое",
+            TaskForce::STATUS_CANCEL => "Отменено",
+            TaskForce::STATUS_WORK => "В работе",
+            TaskForce::STATUS_READY => "Выполнено",
+            TaskForce::STATUS_FAILED => "Провалено"
+        ];
+        if (!$status) {
+            return $statusMap;
+        }
+        return $statusMap[$status] ?? $statusMap;
     }
 
-    private $idPerformer = [];
-    private $idClient = [];
-    private function __construct ($idPerformer, $idClient) {
-        $this->idPerformer = $idPerformer;
-        $this->idClient = $idClient;
+    public function availableStatus (string $status = '')
+    {
+        $statusMap = [
+            TaskForce::STATUS_NEW => [
+                TaskForce::STATUS_CANCEL,
+                TaskForce::STATUS_WORK
+            ],
+            TaskForce::STATUS_WORK => [
+                TaskForce::STATUS_READY,
+                TaskForce::STATUS_FAILED
+            ]
+        ];
+
+        if (!$status) {
+            return $statusMap;
+        }
+        return $statusMap[$status] ?? [];
     }
 
+    public function setStatus (string $status)
+    {
+        $availableStatus = $this->availableStatus($this->_activeStatus);
+        if (in_array($status, $availableStatus)) {
+            $this->_activeStatus = $status;
+            return $status;
+        }
+        return $this->_activeStatus;
     }
+
+
+}
+
