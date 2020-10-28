76f060faee9336b437cbdab66bfae67691a8047b
Влад <poputchik07@mail.ru>
2020-10-28 19:33:23 +0300

    Corrected

--- schema.sql
+++ schema.sql
@@ -4,67 +4,92 @@ CREATE DATABASE taskforce
 
 USE taskforce;
 
-CREATE TABLE user (
+CREATE TABLE users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    id_category VARCHAR(100),
-    date_reg DATETIME,
-    name VARCHAR(200) NOT NULL,
-    email VARCHAR(150) NOT NULL UNIQUE,
-    phone VARCHAR(11) NOT NULL UNIQUE,
-    skype VARCHAR(100),
-    telegram VARCHAR(100),
-    img VARCHAR(250),
+    category_id VARCHAR(25) NOT NULL,
+    date_reg DATETIME NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL UNIQUE,
+    phone VARCHAR(11) NOT NULL,
+    skype VARCHAR(40) NOT NULL,
+    telegram VARCHAR(40) NOT NULL,
+    img VARCHAR(250) NOT NULL,
     birthday DATETIME NOT NULL,
-    id_city INT,
-    about VARCHAR(200),
-    rating INT,
-    password TEXT,
-    FOREIGN KEY (id_city) REFERENCES cities(id),
-    FOREIGN KEY (id_category) REFERENCES categories(id)
+    city_id INT NOT NULL,
+    about VARCHAR(200) NOT NULL,
+    rating INT NOT NULL,
+    failed_task INT NOT NULL,
+    complete_task INT NOT NULL,
+    password_hash VARCHAR(250) NOT NULL,
+    FOREIGN KEY (city_id) REFERENCES cities(id),
+    FOREIGN KEY (category_id) REFERENCES categories(id)
 );
 
 CREATE TABLE categories (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(120) NOT NULL,
-    code VARCHAR(100) NOT NULL UNIQUE
+    name VARCHAR(50) NOT NULL,
+    code VARCHAR(10) NOT NULL UNIQUE
 );
 
 CREATE TABLE cities (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(120)
+    name VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE task (
+CREATE TABLE tasks (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    date_create DATETIME,
-    title VARCHAR(150),
-    description TEXT(5000),
-    img VARCHAR(250),
+    date_create DATETIME NOT NULL,
+    title VARCHAR(50) NOT NULL,
+    description TEXT NOT NULL,
+    img VARCHAR(250) NOT NULL,
     status INT NOT NULL,
-    price INT,
+    price INT NOT NULL,
     date_end DATE,
-    id_user INT NOT NULL,
-    id_category INT  NOT NULL,
-    id_city INT,
+    user_id INT NOT NULL,
+    category_id INT NOT NULL,
+    city_id INT NOT NULL,
     location VARCHAR(100),
-    id_status INT NOT NULL,
-    FOREIGN KEY (id_user) REFERENCES user(id),
-    FOREIGN KEY (id_city) REFERENCES cities(id),
-    FOREIGN KEY (id_category) REFERENCES categories(id),
-    FOREIGN KEY (id_status) REFERENCES status(id)
+    status_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (city_id) REFERENCES cities(id),
+    FOREIGN KEY (category_id) REFERENCES categories(id)
 );
 
-CREATE TABLE status (
+/*
+status_id статусы заданий
+1-New,
+2-Cancel,
+3-Work,
+4-Ready,
+5-Failed
+*/
+
+--Заполнение с помощью json
+CREATE TABLE settings (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(120)
+    setting VARCHAR(50) NOT NULL
+);
+
+
+
+CREATE TABLE files (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    users_id INT NOT NULL,
+    file_1 VARCHAR(250),
+    file_2 VARCHAR(250),
+    file_3 VARCHAR(250),
+    file_4 VARCHAR(250),
+    file_5 VARCHAR(250),
+    file_6 VARCHAR(250),
+    FOREIGN KEY (users_id) REFERENCES users(id)
 );
 
 CREATE TABLE reviews (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    id_user INT NOT NULL,
-    id_task INT NOT NULL,
-    rating INT,
-    review TEXT,
-    FOREIGN KEY (id_user) REFERENCES user(id),
-    FOREIGN KEY (id_task) REFERENCES task(id)
+    user_id INT NOT NULL,
+    task_id INT NOT NULL,
+    rating INT NOT NULL,
+    review TEXT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (task_id) REFERENCES tasks(id)
 );
